be657cf4b61902961fa7a04bc9a42645c155e900
Shrey Patel <shreynpatel23@gmail.com>
2024-03-27 15:55:36 -0700

    Add project files.

---
+++ GameDevAgency/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameDevAgency.Startup))]
+namespace GameDevAgency
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
